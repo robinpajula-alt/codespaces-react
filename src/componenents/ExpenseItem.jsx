331541a46eca56c2ffbbfc3aec3099e9b95fd214
Robin Pajula <robin.pajula@voco.ee>
2026-09-02 08:44:25 +0000

    3. poole peale jõudnud

--- src/componenents/ExpenseItem.jsx
+++ src/componenents/ExpenseItem.jsx
@@ -1,12 +1,13 @@
 import "./ExpenseItem.css"
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+    console.log(props)
     return (
     <div className="expense-item">
-        <div>Date</div>
+        <div>{date.toString()}</div>
         <div className="expense-item__description">
-            <h2>Title</h2>
-            <div className="expense-item__price">Price</div>
+            <h2>{title}</h2>
+            <div className="expense-item__price">{price}</div>
         </div>
     </div>)
 };
