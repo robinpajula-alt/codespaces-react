a5a711f1a5866c1f13e2329ed3135cecfe5bf899
Robin Pajula <robin.pajula@voco.ee>
2026-09-14 08:32:39 +0000

    10. Sündmuste töötlus komponentides

--- src/componenents/Expenses/ExpenseItem.jsx
+++ src/componenents/Expenses/ExpenseItem.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card.jsx";
+import Card from "../Card.jsx";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.expenseData.title);
+  const clickHandler = () => {
+    console.log("Clicked!");
+    setTitle(`Updated by click ${title}`);
+    console.log(title);
+  }
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.data.date}/>
+      <ExpenseDate date={props.expenseData.date}/>
       <div className="expense-item__description">
-        <h2>{props.data.title}</h2>
-        <div className="expense-item__price">${props.data.price}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.expenseData.price}</div>
       </div>
+      <button onClick={clickHandler}>Click me</button>
     </Card>
   );
 };
--- src/componenents/Expenses/Expenses.jsx
+++ src/componenents/Expenses/Expenses.jsx
@@ -1,12 +1,12 @@
 import ExpenseItem from "./ExpenseItem.jsx";
-import Card from "../UI/Card.jsx";
+import Card from "../Card.jsx";
 import "./Expenses.css";
 
 const Expenses = (props) => {
   return (
     <Card className="expenses">
-      <ExpenseItem data={props.expenses[0]} />
-      <ExpenseItem data={props.expenses[1]} />
+      <ExpenseItem expenseData={props.expenses[0]} />
+      <ExpenseItem expenseData={props.expenses[1]} />
     </Card>
   );
 };
