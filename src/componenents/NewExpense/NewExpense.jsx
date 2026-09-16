2b12f7b15e23f76adb2e2ad00dbc1232bbcdcd9a
Robin Pajula <robin.pajula@voco.ee>
2026-09-16 06:33:48 +0000

    11. Vormitöötlus Reactis

--- src/componenents/NewExpense/NewExpense.jsx
+++ src/componenents/NewExpense/NewExpense.jsx
@@ -7,7 +7,7 @@ const NewExpense = (props) => {
             ...enteredExpenseData,
             id: Math.random().toString(),
         };
-        console.log(expenseData);
+        props.onAddExpense(expenseData);
     };
     return (
         <div className="new-expense">
